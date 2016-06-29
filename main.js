dc61927b47bd8fe83a40e824e392b8a958deb9bd
Caryn Ligon <carynligon@gmail.com>
2016-06-29 15:15:45 -0500

    styling fixes

--- main.js
+++ main.js
@@ -5,24 +5,34 @@ var $filesCategory = $('#files .first');
 var $mailCategory = $('#mail .first');
 var $cloudCategory = $('#cloud .first');
 var $allDropdowns = $('.dropdown');
+var $allCategories = $('.first');
 
 $filesDropdown.addClass('dropdown-hide');
 $mailDropdown.addClass('dropdown-hide');
 $cloudDropdown.addClass('dropdown-hide');
 
 $filesCategory.on('click', function () {
+  $(this).toggleClass('active');
+  $mailCategory.removeClass('active');
+  $cloudCategory.removeClass('active');
   $filesDropdown.toggleClass('dropdown-hide');
   $mailDropdown.addClass('dropdown-hide');
   $cloudDropdown.addClass('dropdown-hide');
 });
 
 $mailCategory.on('click', function () {
+  $(this).toggleClass('active');
+  $filesCategory.removeClass('active');
+  $cloudCategory.removeClass('active');
   $mailDropdown.toggleClass('dropdown-hide');
   $filesDropdown.addClass('dropdown-hide');
   $cloudDropdown.addClass('dropdown-hide');
 });
 
 $cloudCategory.on('click', function () {
+  $(this).toggleClass('active');
+  $mailCategory.removeClass('active');
+  $filesCategory.removeClass('active');
   $cloudDropdown.toggleClass('dropdown-hide');
   $mailDropdown.addClass('dropdown-hide');
   $filesDropdown.addClass('dropdown-hide');
